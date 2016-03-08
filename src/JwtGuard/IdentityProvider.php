648d96c65da5a4ef60b4bb0b34c2a98ff5543518
Horváth Péter <peter@clearbooks.co.uk>
2016-03-08 15:51:58 +0100

    read segments from JWT

--- src/JwtGuard/IdentityProvider.php
+++ src/JwtGuard/IdentityProvider.php
@@ -3,9 +3,23 @@ namespace Clearbooks\Dilex\JwtGuard;
 
 interface IdentityProvider
 {
+    /**
+     * @return string
+     */
     public function getUserId();
 
+    /**
+     * @return string
+     */
     public function getGroupId();
 
+    /**
+     * @return bool
+     */
     public function getIsAdmin();
+
+    /**
+     * @return array
+     */
+    public function getSegments();
 }
